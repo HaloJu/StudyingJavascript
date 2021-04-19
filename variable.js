1c38a63ada7547f62bb86a30d88a19c737a3a06c
haloju <halojuya@gmail.com>
2021-04-19 15:16:54 +0900

    3 primitive type, object type, Dynamic typing

--- variable.js
+++ variable.js
@@ -57,3 +57,51 @@ console.log(Nan);
 const bigInt = 1234567890123456789012345678901234567890n; //over (-2^53 ~ 2^53)
 console.log(`value: ${bigInt}, type: ${typeof bigInt}`);
 Number.MAX_SAFE_INTEGER;
+
+//string
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello ' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brendan}!`; //template literals (string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+console.log('value: ' + helloBob + ' type: ' + typeof helloBob);
+
+//boolean
+//false:0, null, undefined, NaN, ''
+//true: any other value
+const canRead = true;
+const test = 3 < 1; //false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+//null
+let nothing = null;
+console.log(`vaule: ${nothing}, type: ${typeof nothing}`);
+
+
+//undefined
+let x; //== let x = undefiend;
+console.log(`vaule: ${x}, type: ${typeof x}`);
+
+//symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);
+
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2);
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`)
+
+// 5. Dynamic typing : dynamically typed langyage
+let text = 'hello'; //text type = string
+console.log(text.charAt(0)); // h
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1; //text type = string => number
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '7' + 5; //string + number => string + string => strig
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '8' / '2'; // string/string => number 
+console.log(`value: ${text}, type: ${typeof text}`);
+console.log(text.charAt(0));
