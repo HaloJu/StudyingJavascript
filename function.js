aec4ce2a829149c7336e3597b0bb36c7b4209636
haloju <halojuya@gmail.com>
2021-04-28 23:59:50 +0900

    5 Function - defalt`rest p(ES6),local scope,Return

--- function.js
+++ function.js
@@ -2,6 +2,28 @@
 // - fundamental building block in the program
 // - subprogram can be used multiple times
 // - performs a task or calculates a value
+
+// 1. Function declaration
+// function name(param1, param2) { body... return; }
+// one function === one thing
+// naming: doSomething, command, verb
+// e.g. createCardAndPoint -> createCard, createPoint
+// function is object in JS
+
+//사실 Hello 밖에 출력되지 않기 때문에 유용하지 않은 함수
+function printHello() {
+    console.log('Hello');
+}
+printHello();
+
+//parameter로 message를 전달하면 전달받은 message를 출력하는 함수
+function log(message) {
+    console.log(message);
+}
+log('Hello@');
+log(1234);
+
+
 function changeName(obj) {
     obj.name = 'coder';
 }
@@ -9,3 +31,69 @@ const ellie1 = { name: 'ellie' };
 changeName(ellie1);
 console.log(ellie1);
 
+
+// 2. Parameters
+// premitive parameters: passed by value
+// object parameters: passed by reference
+function changeName(obj) {
+    obj.name = 'coder';
+}
+const ellie2 = { name: 'ellie' };
+changeName(ellie2);
+console.log(ellie2);
+
+
+
+// 3. Default parameters (added in ES6)
+function showMessage(message, from = 'unknown') {
+    console.log(`${message} by ${from}`);
+}
+showMessage(`Hi!`);
+
+
+
+// 4. Rest parameters (added in ES6)
+function printAll(...args) {
+    for (let i = 0; i < args.length; i++) {
+        console.log(args[i]);
+    }
+}
+printAll('dream', 'coding', 'ellie');
+
+
+
+// 5. Local scope
+let globalMessage = 'global'; // global variable
+function printMessage() {
+    let message = 'hello';
+    console.log(message); //local variable
+    console.log(globalMessage);
+}
+printMessage();
+console.log(message);
+
+
+// 6. Return a value
+function sum(a, b) {
+    return a + b;
+}
+const result = sum(1, 2); // 3
+console.log(`sum: ${sum(1, 2)}`);
+
+
+// 7. Early return, early exit 
+
+// bad
+function upgradeUser(user) {
+    if (user.point > 10) {
+        //long upgrade logic...
+    }
+}
+
+// good
+function upgradeUser(user) {
+    if (user.point <= 10) {
+        return;
+    }
+    //long upgrade logic...
+}
