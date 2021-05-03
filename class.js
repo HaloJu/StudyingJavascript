9c0c3c21b3ecb0a15f87065d4cb21b84c9ef0015
haloju <halojuya@gmail.com>
2021-05-03 16:49:21 +0900

    6 Class - getter and setter, fields(public, private), static

---
+++ class.js
@@ -0,0 +1,50 @@
+'use strict';
+// Object-oriented programming
+// class: template
+// object: instance of a class
+// JavaScript classes
+//  - introduced in ES6
+//  - syntactical sugar over prototype-based inheritance
+
+// 1. Class declarations
+class Person {
+    //constructor
+    constructor(name, age) {
+        //fields
+        this.name = name;
+        this.age = age;
+    }
+
+    //methods
+    speak() {
+        console.log(`${this.name}: hello!`);
+    }
+}
+
+const halo = new Person('juya', '20');
+console.log(halo.name);
+console.log(halo.age);
+halo.speak();
+
+
+// 2. Getter and setters
+class User {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    get age() {
+        return this._age
+    }
+    set age(value) {
+        // if (value < 0) {
+        //     throw Error('age can not be negative');
+        // }
+        this._age = value < 0 ? 0 : value;
+    }
+}
+
+const user1 = new User('Steve', 'job', -1);
+console.log(user1.age);
