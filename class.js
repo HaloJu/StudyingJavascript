69654622f95ae48e8c3c0d727823cae1fd52d807
haloju <halojuya@gmail.com>
2021-05-04 16:42:17 +0900

    6 Class - inheritance

--- class.js
+++ class.js
@@ -48,3 +48,77 @@ class User {
 
 const user1 = new User('Steve', 'job', -1);
 console.log(user1.age);
+
+
+
+// 3. Fields (public, private)
+// Too soon!
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Class_fields
+class Experiment {
+    publicField = 2;
+    #privateField = 2;
+}
+const experiment = new Experiment();
+console.log(experiment.publicField);
+console.log(experiment.privateField);
+
+
+
+// 4. Static properties and methods 
+// Too soon!
+class Article {
+    static publisher = 'Dream Coding';
+    constructor(articleNumber) {
+        this.articleNumber = articleNumber;
+    }
+
+    static printPublisher() {
+        console.log) (Article.publisher);
+    }
+}
+
+const article1 = new Article(1);
+const article2 = new Article(2);
+
+
+
+// 5. Inheritance
+// a way for one class to extend another class.
+class Shape {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw() {
+        console.log(`drawing ${this.color} color!`);
+    }
+
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+class Rectangle extends Shape { }
+class Triangle extends Shape {
+    draw() {
+        super.draw();
+        console.log('🔺');
+    }
+    getArea() {
+        return (this.width * this.height) / 2;
+    }
+    toString() {
+        return `Triangle: color: ${this.color}`;
+    }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw();
+console.log(ractangle.getArea());
+
+const triangle = new Triangle(20, 20, 'red');
+triangle.draw();
+console.log(triangle.getArea());
+
